7d4aebdef0293007af936f27580f3b10da8775f4
Radouane0 <ikramaboufera@gmail.com>
2023-10-04 18:58:33 +0200

    Ajout changement de quantité des produits

--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -1,19 +1,40 @@
-import React from 'react'
-import './ProductsList.css'
+import React, { useState } from 'react';
+import './ProductsList.css';
 
 const ProductsList = ({ products }) => {
+    const [quantities, setQuantities] = useState(new Array(products.length).fill(0));
+
+    const handleQuantityIncreased = (index) => {
+        const newQuantities = [...quantities];
+        newQuantities[index] += 1;
+        setQuantities(newQuantities);
+    };
+
+    const handleQuantityDecreased = (index) => {
+        if(quantities[index] > 0) {
+            const newQuantities = [...quantities];
+            newQuantities[index] -= 1;
+            setQuantities(newQuantities);
+        }
+    }
+
     return (
         <div className='productsList'>
-           {products.map((product) => (
-            <div key={product.id} className='product'>
-                <img src={product.image} alt={product.name} />
-                <div className="product-infos">
-                    <h2>{product.name}</h2>
-                    <p>{product.price} €</p>
-                    <button className="add-product">Ajouter au panier</button>
-                </div>     
-            </div>
-           ))}   
+            {products.map((product, index) => (
+                <div key={product.id} className='product'>
+                    <img src={product.image} alt={product.name} />
+                    <div className="product-infos">
+                        <h2>{product.name}</h2>
+                        <p>{product.price} €</p>
+                        <div className="quantity-controls"> 
+                            <button className='reduce-quantity' onClick={() => handleQuantityDecreased(index)}>-</button>
+                            <p>Quantity : {quantities[index]}</p>
+                            <button className='add-quantity' onClick={() => handleQuantityIncreased(index)}>+</button>
+                        </div>
+                        <button className="add-product">Ajouter au panier</button>
+                    </div>
+                </div>
+            ))}
         </div>
     );
 };
