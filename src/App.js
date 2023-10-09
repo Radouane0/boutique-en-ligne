be0f3f93c145ecbe06f209f789a7e629e126ccb7
Radouane0 <ikramaboufera@gmail.com>
2023-10-09 17:14:57 +0200

    Ajout Loader

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Header from './components/Header/Header'
-import Home from './Pages/Home/Home'
-import Panier from './Pages/Panier/Panier'
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './Pages/Home/Home';
+import Panier from './Pages/Panier/Panier';
+import Loader from './components/Loader/Loader';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
 
   return (
     <Router>
       <div className="app">
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/panier' element={<Panier />} />
-        </Routes>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <div>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/panier" element={<Panier />} />
+            </Routes>
+          </div>
+        )}
       </div>
     </Router>
-  )
+  );
 }
 
 export default App;
