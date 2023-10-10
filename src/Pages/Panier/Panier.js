b1aabe45faef9ebb02ba96d7cf65730d0d5af633
Radouane0 <ikramaboufera@gmail.com>
2023-10-10 18:03:56 +0200

    Affichage produit dans panier

--- src/Pages/Panier/Panier.js
+++ src/Pages/Panier/Panier.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
+import './Panier.css'
+
 const Panier = () => {
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+    const calculateTotalPrice = (product) => {
+        return product.quantity * product.price;
+    }
+
+
     return (
-        <div>
-            Hello World !
+        <div className='panier'>
+            <h2>Panier</h2>
+            <ul>
+                {cart.map((item, index) => (
+                    <li key={index}>
+                        <img src={item.image} alt='Produit'></img>
+                        <p>{item.name} - Quantité: {item.quantity} - Prix unitaire: {item.price} € - Prix total: {calculateTotalPrice(item)} €</p>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './ProductsList.css';
 
 const ProductsList = ({ products }) => {
     const [quantities, setQuantities] = useState(new Array(products.length).fill(0));
+    const [cart, setCart] = useState([]);
 
     const handleQuantityIncreased = (index) => {
         const newQuantities = [...quantities];
@@ -19,6 +20,22 @@ const ProductsList = ({ products }) => {
         }
     }
 
+    const handleAddToCart = (product, index) => {
+        const cartItem = {
+            image: product.image,
+            name: product.name,
+            quantity: quantities[index],
+            price: product.price,
+        };
+
+        const updatedCart = [...cart, cartItem];
+        setCart(updatedCart);
+    };
+
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }, [cart]);
+
     return (
         <div className='productsList'>
             {products.map((product, index) => (
@@ -32,7 +49,7 @@ const ProductsList = ({ products }) => {
                             <p>Quantity : {quantities[index]}</p>
                             <button className='add-quantity' onClick={() => handleQuantityIncreased(index)}>+</button>
                         </div>
-                        <button className="add-product">Ajouter au panier</button>
+                        <button className="add-product" onClick={() => handleAddToCart(product, index)}>Ajouter au panier</button>
                     </div>
                 </div>
             ))}
